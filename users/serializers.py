edba85d26932b54d85934043652a6c50c96a46b4
Manu <napestershine@gmail.com>
2021-12-07 07:38:28 +0100

    refactored

--- users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_frameowrk import serializers
-from models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = User
-        fields = ('id', 'email', 'username', 'address', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
